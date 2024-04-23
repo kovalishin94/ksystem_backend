d6c3749afec52a9bbb6dcb26f6dc7ebdec1c7f62
Evgeniy Kovalishin <kovalish94@mail.ru>
2024-04-23 04:02:56 +0000

    asgi.py: config for deployment

--- ksystem_backend/asgi.py
+++ ksystem_backend/asgi.py
@@ -6,19 +6,22 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ksystem_backend.settings')
+django_asgi_app = get_asgi_application()
+
 from chat.consumers import ChatConsumer
 from security.middleware import JwtAuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ksystem_backend.settings')
-
 application = ProtocolTypeRouter(
     {
-        'http': get_asgi_application(),
+        'http': django_asgi_app,
         'websocket': AllowedHostsOriginValidator(
             JwtAuthMiddlewareStack(
                 URLRouter(
                     [
-                        re_path(r"ws/chat/(?P<room_name>[^/]+)/$", ChatConsumer.as_asgi()),
+                        re_path(
+                            r"ws/chat/(?P<room_name>[^/]+)/$", ChatConsumer.as_asgi()),
                     ]
                 )
             )
