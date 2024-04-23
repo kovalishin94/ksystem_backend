2d6ccf526eb1643a3b6ea4f9bf532b28825db70f
Evgeniy Kovalishin <kovalish94@mail.ru>
2024-04-23 04:51:01 +0000

    chat:consumers: cancel sendind messages on connect

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,9 +5,9 @@ from django.utils.timesince import timesince
 
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-from asgiref.sync import sync_to_async
+# from asgiref.sync import sync_to_async
 
-from .sql import message_list_sql
+# from .sql import message_list_sql
 from .models import Message, Chat
 
 
@@ -19,9 +19,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
-        self.messages = await sync_to_async(message_list_sql)(self.room_name)
+        # self.messages = await sync_to_async(message_list_sql)(self.room_name)
 
-        await self.send(text_data=json.dumps(self.messages, default=str))
+        # await self.send(text_data=json.dumps(self.messages, default=str))
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
