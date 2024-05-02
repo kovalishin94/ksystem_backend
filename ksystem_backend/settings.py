68eda51f86e5e665d135fb56e6c06245c2d5f02a
Evgeniy Kovalishin <kovalish94@mail.ru>
2024-05-02 04:41:40 +0000

    settings:access - 180m, refresh 10 d

--- ksystem_backend/settings.py
+++ ksystem_backend/settings.py
@@ -112,7 +112,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=180),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
 }
 
 CORS_ALLOWED_ORIGINS = [
